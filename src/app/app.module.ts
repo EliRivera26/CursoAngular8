7246f7075454563c767e42fc7030aea28486bc17
Elizabeth Rivera <elizabeth.trigueros10@gmail.com>
2019-12-19 13:47:14 -0600

    Página web que agrega usuarios y los muestra en una tabla.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ChangeDetectorRef } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { FilterPipe } from './pipes/filter.pipe';
+import { UsuariosPipe } from './pipes/usuarios.pipe';
+import { NgbModule, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgFallimgModule } from 'ng-fallimg';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe,
+    UsuariosPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    NgFallimgModule.forRoot({
+      default:'/assets/picture1.png',
+      profile: '/assets/default-profile-image.png',
+      post: '/assets/default-post-image.png'
+    })
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
