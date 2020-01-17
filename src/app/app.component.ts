d7c9d763893fc0c1c26e6bea52f5c8bc6b4260eb
Elizabeth <elizabeth.rivera@gingroupcontraloria>
2020-01-17 17:19:10 -0600

    Se agregó la función detectChanges(); para actualizar los cambios sin refrescar la página. Se modificó el modal para mostrar el mensaje de error.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,3 @@
-import { Component, ChangeDetectorRef, ChangeDetectionStrategy, ViewChild, Inject, OnChanges } from '@angular/core';
-import { NgbModal, ModalDismissReasons, NgbModalOptions, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
-import { faUser, faUserEdit, faTrashAlt, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { filter, map, retry } from 'rxjs/operators';
-import { text } from '@fortawesome/fontawesome-svg-core';
-
-
 @Component({
   selector: 'app-root',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,12 +6,12 @@ import { text } from '@fortawesome/fontawesome-svg-core';
 })
 
 export class AppComponent {
-  form: FormGroup;
+  formUsuario: FormGroup;
   _usuarios = [];
   _usuario = {};
   searchText = '';
   modal:NgbModalRef;
-  enviado:boolean;
+  enviado = false;
   usuarios=[];
   formValues: any;
   reset = false;
@@ -31,115 +22,131 @@ export class AppComponent {
   closeResult: string;
   modalOptions:NgbModalOptions;
 
+    constructor(
+      private modalService: NgbModal, private fb: FormBuilder, private http: HttpClient, private ref: ChangeDetectorRef
+    ){
+        this.modalOptions = {
+        backdrop:'static',
+        backdropClass:'customBackdrop'
+      }
+
+        this.formUsuario = this.fb.group({
+        nombre: new FormControl('', [Validators.required]),
+        apellido: new FormControl('', [Validators.required]),
+        tel: new FormControl('', [Validators.required, Validators.maxLength(10), Validators.minLength(10)]),
+        edad: new FormControl('', [Validators.required, Validators.maxLength(1)]),
+      });
+
+      
+      this.getUsuarios("Id");
+    }
+
+    get fu() { return this.formUsuario.controls; }
+
+    onReset(modal) {
+      this.enviado = false;
+      this.formUsuario.reset();
+      this._usuario = {};
+      modal.close();
+    }
+
 
 //----------------Agregar usuario----------------
 
-  async adduser(mimodal, usuario) {
-    
-    if(this.form.valid){
-      this.enviado = true;
-      var x;
+    adduser(mimodal,usuario) {
       
-      if(usuario.id){
-        x = await this.http.put<Usuario>(this.baseUrl + "Values/"+ usuario.id, usuario).toPromise();
-        console.log(x)
-        //this._usuario=usuario;
-        this.getUsuarios();
-      }
-      else{
-        x = await this.http.post<Usuario>(this.baseUrl + "Values", usuario).toPromise()
-        console.log(x)
-        
-      }
+      this.enviado = true;
+      if(this.formUsuario.invalid){
+        return;
+      }      
+        var servicio;
+          
+            if(usuario.id){
 
-      usuario = x;
-      this.usuarios.push(JSON.parse(JSON.stringify(this._usuario)));
-      this._usuario = {};
-      this.reset = false;
-      this.filtrar();
-      mimodal.close();     
+              servicio = this.http.put<Usuario>(this.baseUrl + "Values/"+ usuario.id, usuario)
+            }else{
+
+              servicio = this.http.post<Usuario>(this.baseUrl + "Values", usuario)
+              
+            }
+
+    servicio.subscribe(x => {
+        if(x.succes){
+            
+            this._usuarios.unshift(JSON.parse(JSON.stringify(x.modelo)));
+            this._usuario = {};
+            this.filtrar();
+            this.ref.detectChanges();
+            this.onReset(mimodal);
+            
+            
+          }else{
+                alert(x.errors);
+          }
+        });     
     }
-
-      mimodal.error = "error no se pudo guardar";     
-  }
 
 
 //------------------Editar usuario--------------- 
 
-  edituser(modal, usuario){
-        
-      this.open(this._usuario);
-      this._usuario = usuario;   
-    }
+  edituser(_usuario, usuario){
+            
+      this.open(_usuario);
+      this._usuario = usuario;  
+  }
 
 
 //-----------------Eliminar usuario---------------
-deletuser(usuarios,modal,index){
-  var service;
-  if(usuarios[index].id){
-    service = this.http.delete<Usuario>(this.baseUrl + "Values/"+ usuarios[index].id); 
-    service.subscribe(x =>
-      {
-        var usuario = x;
-        usuarios.splice(index,1);
-        modal.close(); 
-      }); 
-  }else{
-    modal.error = "no se puede eliminar";
-  }    
-}
+  deletuser(usuarios,modal,index){
+    var service;
+    if(usuarios[index].id){
+      service = this.http.delete<Usuario>(this.baseUrl + "Values/"+ usuarios[index].id); 
+      service.subscribe(x =>
+        {
+          var usuario = x;
+          this.usuarios.splice(index,1);
+          this.ref.detectChanges();
+          modal.close(); 
+        }); 
+    }else{
+      modal.error = "no se puede eliminar";
+    }    
+  }
 
 //---------------Filtrar usuario-----------------
-  filtrar(){
-    this.usuarios = this._usuarios.filter(user => user.nombre.includes(this.searchText));
-  }
-
-  constructor(
-    private modalService: NgbModal, private fb: FormBuilder, private http: HttpClient, 
-  ){
-      this.modalOptions = {
-      backdrop:'static',
-      backdropClass:'customBackdrop'
+    filtrar(){
+      this.usuarios = this._usuarios.filter(user => user.nombre.includes(this.searchText));
     }
 
-      this.form = this.fb.group({
-      nombre: new FormControl('', [Validators.required]),
-      apellido: new FormControl('', [Validators.required]),
-      tel: new FormControl('', [Validators.required,Validators.minLength(10), Validators.maxLength(10)]),
-      edad: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]),
-    });
-
-    this.getUsuarios();
-  }
 
 //-------------Aquí se muestran los usuarios------------ 
-  getUsuarios() {
-    return this.http.get<Usuario[]>(this.baseUrl + "Values")
-    .subscribe(x => 
-      { 
-        this._usuarios = x;
-        this.usuarios=JSON.parse(JSON.stringify(x));  
-        //this.usuariosfiltrados=JSON.parse(JSON.stringify(x))
-      });
-  }
+    getUsuarios(order) {
+      return this.http.get<Usuario[]>(this.baseUrl + "Values?order="+ order)
+      .subscribe(x => 
+        { 
+          this._usuarios = x;
+          this.usuarios=JSON.parse(JSON.stringify(x)); 
+          this.ref.detectChanges(); 
+          //this.usuariosfiltrados=JSON.parse(JSON.stringify(x))
+        });
+    }
 
 //------------------Abrir moodal-------------
-  open(content) {
-    this.modalService.open(content, this.modalOptions).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
- 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
+    open(content) {
+      this.modalService.open(content, this.modalOptions).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+    }
+  
+    private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return  `with: ${reason}`;
+      }
     }
   }
-}
-
